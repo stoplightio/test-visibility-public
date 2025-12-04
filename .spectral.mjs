6e566eb7b41551acfd4df3cc320cdb392f1948f1
SB-ChetanKorde <chetan.korde@smartbear.com>
2025-12-04 17:28:39 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight-local.com:8443/api/v1/projects/cHJqOjI3/spectral.js?branch_slug=main&token=498359cf-9c4f-462e-b160-72bf33b60d3d";
+export default { extends: ruleset };
